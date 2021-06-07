fdab74ac1624741490b06ee725a9108146dbc1b5
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-06-07 13:51:30 +0200

    get tablet  max size

--- tablet_server/main.go
+++ tablet_server/main.go
@@ -22,7 +22,7 @@ var httpClient HttpClient
 
 func cron(httpClient *HttpClient) {
 	gocron.Every(1).Minute().Do(httpClient.SendUpdatesToGFS)
-	<- gocron.Start()
+	<-gocron.Start()
 }
 
 func justPrintErr(err error) {
@@ -40,24 +40,24 @@ func main() {
 	port, _ = strconv.Atoi(os.Args[1])
 
 	// Get max cap
-	max_tablet_cap, _ = strconv.Atoi(os.Getenv("MAX_TAB_CAP"))
+	max_tablet_cap, _ = strconv.Atoi(os.Args[2])
 
 	// Setup logger
-	log_file, err := os.OpenFile("tablet_server.log", os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0644)
-	if (err != nil) {
+	log_file, err := os.OpenFile("tablet_server.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
 		panic(err)
 	}
 	defer log_file.Close()
 	log.SetOutput(log_file)
 
 	// Setup update logs
-	update_logs_file, err := os.OpenFile("updates.log", os.O_RDWR | os.O_CREATE | os.O_TRUNC, 0644)
-	if (err != nil) {
+	update_logs_file, err := os.OpenFile("updates.log", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
 		panic(err)
 	}
 	defer update_logs_file.Close()
 	update_logger = SafeUpdateLog{file: update_logs_file}
-	httpClient = HttpClient{ updateLogger: &update_logger }
+	httpClient = HttpClient{updateLogger: &update_logger}
 
 	// Get server number
 	for server_id < 0 {
@@ -68,13 +68,13 @@ func main() {
 	log.Println(fmt.Sprintf("Got id %v from master", server_id))
 
 	// Create the repository service and bind the update logger
-	repo := Repository{data: BigTablePartition{}, keys: []RowKeyType{}, httpClient: &httpClient, updateLogsFile: &update_logger}	
-	
+	repo := Repository{data: BigTablePartition{}, keys: []RowKeyType{}, httpClient: &httpClient, updateLogsFile: &update_logger}
+
 	// Run the update logger sender cron
 	go cron(&httpClient)
 
 	log.Println("Tablet Server Started")
-	
+
 	// Create the API and bind the repo
 	InitApi(fmt.Sprintf(":%v", port), &repo, log_file)
 }
